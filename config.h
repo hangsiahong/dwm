0cea37c73915943b0ec7cb0c41e701536c7764af
hangsiahong <honghangsia@gmail.com>
2024-09-04 17:10:07 +0700

    updaete

--- config.h
+++ config.h
@@ -22,9 +22,9 @@ static char *fonts[]          = { "MartianMono Nerd Font:size=10", "NotoColorEmo
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
-static char selfgcolor[]            = "#eeeeee";
+static char selfgcolor[]            = "#569567";
 static char selbordercolor[]        = "#770000";
-static char selbgcolor[]            = "#005577";
+static char selbgcolor[]            = "#222222";
 static char *colors[][3] = {
        /*               fg           bg           border   */
        [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
